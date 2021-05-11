267946b5e0cb45f15415ffa6f4c5a15c939a53a9
mac <mac@iMac-cua-mac.local>
2021-05-11 14:57:20 +0700

    update new contract

--- contracts/TuringBonus.sol
+++ contracts/TuringBonus.sol
@@ -1,11 +1,181 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-04-13
+*/
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.12;
 
-import './interfaces/ITuringFarm.sol';
-import './interfaces/IPancakeSwapRouter.sol';
-import './library/SafeMath.sol'; 
+interface IBEP20 {
+    function mint(address account, uint256 amount) external;
+    function burn(address account, uint256 amount) external;
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
 
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract ITuringFarm {
+
+   uint256 public totalShare;
+
+   IBEP20 public want;
+
+   mapping(address => uint256) public shareOf; 
+}
+interface IPancakeSwapRouter {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) 
+        external
+        returns (uint[] memory amounts);
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+}
+
+library SafeMath {
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    require(c / a == b, 'INVALID_MUL');
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0, 'INVALID_DIV'); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a, 'INVALID_SUB');
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a, 'INVALID_ADD');
+    return c;
+  }
+}
 contract TuringBonus {
 
     uint256 public version = 100;
@@ -110,14 +280,26 @@ contract TuringBonus {
         _timelock.queuedTransactions = false;
     }
 
+    function setPancakeSwapContract() public onlyOwner {
+
+        TimeLock storage _timelock = timeLockOf[keccak256(abi.encode('setPancakeSwapContract'))];
+
+        _validateTimelock(_timelock);
+        require(_timelock.addressOf[keccak256(abi.encode('pancakeSwap'))] != address(0), "INVALID_ADDRESS");
+
+        pancakeSwap = IPancakeSwapRouter(_timelock.addressOf[keccak256(abi.encode('pancakeSwap'))]);
+        delete _timelock.addressOf[keccak256(abi.encode('pancakeSwap'))];
+        _timelock.queuedTransactions = false;
+    }
+
     function setBonusHolderConfig() public onlyOwner {
 
         TimeLock storage _timelock = timeLockOf[keccak256(abi.encode('setBonusHolderConfig'))];
 
         _validateTimelock(_timelock);
 
-    	HOLD_BONUS_PERCENT = _timelock.uintOf[keccak256(abi.encode('HOLD_BONUS_PERCENT'))];
-    	HOLD_BONUS_POINT = _timelock.uintOf[keccak256(abi.encode('HOLD_BONUS_POINT'))];
+        HOLD_BONUS_PERCENT = _timelock.uintOf[keccak256(abi.encode('HOLD_BONUS_PERCENT'))];
+        HOLD_BONUS_POINT = _timelock.uintOf[keccak256(abi.encode('HOLD_BONUS_POINT'))];
         delete _timelock.uintOf[keccak256(abi.encode('HOLD_BONUS_PERCENT'))];
         delete _timelock.uintOf[keccak256(abi.encode('HOLD_BONUS_POINT'))];
         _timelock.queuedTransactions = false;
