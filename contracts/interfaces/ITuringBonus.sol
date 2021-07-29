7385d62a547b298fc2fdbfdee41f511760a5ba88
tora <toramisu.c@gmail.com>
2021-07-29 17:49:30 +0700

    fix: compilation and some clean up
    fix: compiler version

--- contracts/interfaces/ITuringBonus.sol
+++ contracts/interfaces/ITuringBonus.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12;
 
+import './ITuringFarm.sol';
+
 interface ITuringBonus {
 	function getBonusAmount(ITuringFarm _turingFarm, address _user, uint256 _turMint) external view returns(uint256);
 }
