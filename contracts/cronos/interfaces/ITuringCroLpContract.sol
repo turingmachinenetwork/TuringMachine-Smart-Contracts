03af6d76e9ccedd71a416e8364e438de96a17e50
Chuong Nguyen <chuongnguyen417@gmail.com>
2022-04-07 17:20:15 +0700

    add token0 interface turing cro LP contract

--- contracts/cronos/interfaces/ITuringCroLpContract.sol
+++ contracts/cronos/interfaces/ITuringCroLpContract.sol
@@ -3,4 +3,5 @@ pragma solidity >=0.6.12;
 
 interface ITuringCrpLpContract {
     function getReserves() external view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast);
+    function token0() external view returns (address);
 }
